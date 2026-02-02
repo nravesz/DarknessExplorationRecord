2a5168aea6c26aaf0285a124bbc2dcd69e585e93
nravesz <nraveszani@gmail.com>
2026-02-02 10:53:22 -0300

    add: configs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
-import GhostStoryList from "./features/GhostStoryList"
+import GhostStoryList from './features/GhostStoryList';
 
 function App() {
-  return     <div className="min-h-screen bg-slate-900 text-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold">
-        Tailwind v4 funciona 🔥
-      </h1>
-    </div>
+	return (
+		<div>
+			<GhostStoryList />
+		</div>
+	);
 }
 
-export default App
+export default App;
--- frontend/src/features/GhostStoryList.tsx
+++ frontend/src/features/GhostStoryList.tsx
@@ -1,5 +1,31 @@
 function GhostStoryList() {
-    return <h1>Hello world</h1>
+	return (
+		<>
+			<h1>Ghost Stories</h1>
+			<table className="border-collapse border border-gray-400 ...">
+				<thead>
+					<tr>
+						<th className="border border-gray-300 ...">State</th>
+						<th className="border border-gray-300 ...">City</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td className="border border-gray-300 ...">Indiana</td>
+						<td className="border border-gray-300 ...">Indianapolis</td>
+					</tr>
+					<tr>
+						<td className="border border-gray-300 ...">Ohio</td>
+						<td className="border border-gray-300 ...">Columbus</td>
+					</tr>
+					<tr>
+						<td className="border border-gray-300 ...">Michigan</td>
+						<td className="border border-gray-300 ...">Detroit</td>
+					</tr>
+				</tbody>
+			</table>
+		</>
+	);
 }
 
 export default GhostStoryList;
