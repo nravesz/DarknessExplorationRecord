133354cd46ab7cab355183248f89a1c0316c8f61
nravesz <nraveszani@gmail.com>
2026-02-06 18:44:56 -0300

    add: consts for the routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './features/Layout';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import { ROUTES } from './routes';
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path={ROUTES.HOME} element={<Home />} />
+        <Route path={ROUTES.LOGIN} element={<Login />} />
       </Route>
     </Routes>
   );
--- frontend/src/features/SideBar.tsx
+++ frontend/src/features/SideBar.tsx
@@ -1,11 +1,12 @@
 import { Home, User, Settings, LogIn } from 'lucide-react';
 import { Navigate, useNavigate } from 'react-router-dom';
+import { ROUTES } from '../routes';
 
 function SideBar() {
   const navigate = useNavigate();
   return (
     <div className="flex flex-col bg-base-200 h-screen w-16 items-center py-4 shadow-lg">
-      <button className="mb-4 p-2 rounded hover:bg-base-300" onClick={() => navigate('/')}>
+      <button className="mb-4 p-2 rounded hover:bg-base-300" onClick={() => navigate(ROUTES.HOME)}>
         <Home size={24} />
       </button>
       <button className="mb-4 p-2 rounded hover:bg-base-300">
@@ -14,7 +15,7 @@ function SideBar() {
       <button className="mb-4 p-2 rounded hover:bg-base-300">
         <Settings size={24} />
       </button>
-      <button onClick={() => navigate('/login')} className="mb-4 p-2 rounded hover:bg-base-300">
+      <button onClick={() => navigate(ROUTES.LOGIN)} className="mb-4 p-2 rounded hover:bg-base-300">
         <LogIn size={24} />
       </button>
     </div>
