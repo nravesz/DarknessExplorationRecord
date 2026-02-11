50292811171cc01b83250845209da541ec83e764
nravesz <nraveszani@gmail.com>
2026-02-11 16:02:13 -0300

    refactor: set the uri in .env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,16 +4,19 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { GhostStoryModule } from './ghost-stories/ghost-stories.module';
 import { UserModule } from './user/user.model';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
 	imports: [
-		MongooseModule.forRoot(
-			'mongodb+srv://nraveszani_db_user:98uTvtGTO3AOK9H7@cluster0.roqnvma.mongodb.net/DarknessExplorationRecord?retryWrites=true&w=majority'
-		),
 		ConfigModule.forRoot({
 			isGlobal: true,
+		}),
+		MongooseModule.forRootAsync({
+			inject: [ConfigService],
+			useFactory: (config: ConfigService) => ({
+				uri: config.get<string>('MONGODB_URI'),
+			}),
 		}),
 		UserModule,
 		GhostStoryModule,
