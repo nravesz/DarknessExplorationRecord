37a5877ab0149442deacee7c68d1218d2eabc0d1
nravesz <nraveszani@gmail.com>
2026-02-12 20:17:32 -0300

    remove: unused imports

--- frontend/src/features/layout/sidebar/SideBar.tsx
+++ frontend/src/features/layout/sidebar/SideBar.tsx
@@ -1,10 +1,8 @@
 import { Home, User, Settings, LogIn } from 'lucide-react';
-import { Navigate, useNavigate } from 'react-router-dom';
 import { ROUTES } from '../../../routes';
 import SideBarButton from './SideBarButton';
 
 function SideBar() {
-  const navigate = useNavigate();
   return (
     <div className="flex flex-col bg-base-200 h-screen w-16 items-center py-4 shadow-lg">
       <SideBarButton icon={Home} route={ROUTES.HOME} />
