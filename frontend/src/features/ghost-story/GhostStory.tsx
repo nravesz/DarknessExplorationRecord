d6bfc88604174499f19125b8b22b9d898ef086c1
nravesz <nraveszani@gmail.com>
2026-02-12 17:58:27 -0300

    add: init

---
+++ frontend/src/features/ghost-story/GhostStory.tsx
@@ -0,0 +1,28 @@
+function GhostStory() {
+  return (
+	<div>
+		<div className="flex justify-between">
+			<h1 className="text-4xl font-bold">Tuesday Quiz Show</h1>
+			<h1 className="text-4xl font-bold">Qterw-D-43</h1>
+		</div>
+
+		<div className="divider"></div>
+
+		<div>
+			<div>ID: Qterw-D-43</div>
+			<div>Class: D</div>
+			<div>It has a procedure manual: yes</div>
+			<div>User access level: add records</div>
+		</div>
+
+		<div className="divider"></div>
+
+		<div>Description</div>
+
+		<div>Manual to deal with this story</div>
+
+		<div>Darkness Exploration Records</div>
+	</div>);
+}
+
+export default GhostStory;
