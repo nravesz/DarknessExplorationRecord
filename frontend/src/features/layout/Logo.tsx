e939ad46631e443c692972c3256ecb1b6dc5f30b
nravesz <nraveszani@gmail.com>
2026-02-12 21:13:33 -0300

    style: separate the logo from the text :P

--- frontend/src/features/layout/Logo.tsx
+++ frontend/src/features/layout/Logo.tsx
@@ -2,7 +2,7 @@ import logo from '../../assets/Baekilmong_Logo.png';
 
 function Logo() {
   return (
-    <div className="flex flex-col items-center w-full">
+    <div className="flex flex-col items-center w-full space-y-2">
       <img src={logo} className="w-20 h-20" />
       <span className="text-sm font-semibold tracking-wide text-center">
         Darkness Exploration Records
