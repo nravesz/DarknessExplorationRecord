963e02582710eac9623bf8c8f8a8d2c2b607d138
nravesz <nraveszani@gmail.com>
2026-03-23 17:48:57 -0300

    add: buttons name

--- frontend/src/features/ghost-story/TabBar.tsx
+++ frontend/src/features/ghost-story/TabBar.tsx
@@ -3,10 +3,10 @@ import './TabBar.css';
 function TabBar() {
   return (
     <div className='flex justify-center'>
-      <button className="slanted-btn">Button</button>
-      <button className="slanted-btn">Button</button>
-      <button className="slanted-btn">Button</button>
-      <button className="slanted-btn">Button</button>
+      <button className="slanted-btn">Overview</button>
+      <button className="slanted-btn">Description</button>
+      <button className="slanted-btn">Manual</button>
+      <button className="slanted-btn">Records</button>
     </div>
   );
 }
