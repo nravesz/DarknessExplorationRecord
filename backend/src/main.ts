292884d80c18c780b469332d02324d89ff0d762a
nravesz <nraveszani@gmail.com>
2026-04-22 16:09:43 -0300

    add: global error handler

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
 	app.useGlobalPipes(new ValidationPipe());
+	app.useGlobalFilters(new HttpExceptionFilter());
 
 	app.enableCors({
 		origin: 'http://localhost:5173',
