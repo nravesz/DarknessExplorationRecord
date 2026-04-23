fbcf6906f62aef59aa40063d8f96b2b3ba1b8b19
nravesz <nraveszani@gmail.com>
2026-04-23 15:22:28 -0300

    fix: not receivenig the token after creating an account

--- frontend/src/features/auth/hooks/useRegister.ts
+++ frontend/src/features/auth/hooks/useRegister.ts
@@ -1,15 +1,21 @@
 import { useMutation } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import { postRegister } from '../api/auth.service';
+import { useAuthContext } from '../../../context/hooks/useAuthContext';
 import { ROUTES } from '../../../routes';
 
 export function useRegister() {
   const navigate = useNavigate();
+  const { setAuth } = useAuthContext();
 
   return useMutation({
     mutationFn: postRegister,
-    onSuccess: () => {
-      navigate(ROUTES.LOGIN);
+    onSuccess: (data) => {
+      localStorage.setItem('accessToken', data.accessToken);
+      localStorage.setItem('codename', data.codename);
+      localStorage.setItem('email', data.email);
+      setAuth(data.codename, data.email);
+      navigate(ROUTES.HOME);
     },
   });
 }
