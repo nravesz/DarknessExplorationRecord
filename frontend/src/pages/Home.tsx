a54e51868e7cc0ca4eaf2602d7064ee472f1dcdb
nravesz <nraveszani@gmail.com>
2026-05-06 16:35:41 -0300

    add: homepage

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,10 @@
 function Home() {
-  return <div>This is the home page</div>;
+  return (
+    <div className="flex flex-col items-center pt-20">
+      <h1 className="text-6xl font-bold text-center">Darkness Exploration Records</h1>
+      <p className="mt-4 text-center text-gray-500">A dashboard to register, monitor and manage any kind of ghost stories</p>
+    </div>
+  );
 }
 
 export default Home;
